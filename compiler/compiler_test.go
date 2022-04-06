7f6ad06f62d7b3b5801f55d123a10775bb7cae35
XiovV <xiovv@tutanota.com>
2022-04-06 18:22:45 +0200

    hashes implemented

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -16,10 +16,55 @@ type compilerTestCase struct {
 	expectedInstructions []code.Instructions
 }
 
+func TestHashLiterals(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input: "{}",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpHash, 0),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: "{1: 2, 3: 4, 5: 6}",
+			expectedConstants: []interface{}{1, 2, 3, 4, 5, 6},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpConstant, 3),
+				code.Make(code.OpConstant, 4),
+				code.Make(code.OpConstant, 5),
+				code.Make(code.OpHash, 6),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: "{1: 2 + 3, 4: 5 * 6}",
+			expectedConstants: []interface{}{1, 2, 3, 4, 5, 6},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpAdd),
+				code.Make(code.OpConstant, 3),
+				code.Make(code.OpConstant, 4),
+				code.Make(code.OpConstant, 5),
+				code.Make(code.OpMul),
+				code.Make(code.OpHash, 4),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
 func TestArrayLiterals(t *testing.T) {
 	tests := []compilerTestCase{
 		{
-			input: "[]",
+			input:             "[]",
 			expectedConstants: []interface{}{},
 			expectedInstructions: []code.Instructions{
 				code.Make(code.OpArray, 0),
@@ -27,7 +72,7 @@ func TestArrayLiterals(t *testing.T) {
 			},
 		},
 		{
-			input: "[1, 2, 3]",
+			input:             "[1, 2, 3]",
 			expectedConstants: []interface{}{1, 2, 3},
 			expectedInstructions: []code.Instructions{
 				code.Make(code.OpConstant, 0),
@@ -38,7 +83,7 @@ func TestArrayLiterals(t *testing.T) {
 			},
 		},
 		{
-			input: "[1 + 2, 3 - 4, 5 * 6]",
+			input:             "[1 + 2, 3 - 4, 5 * 6]",
 			expectedConstants: []interface{}{1, 2, 3, 4, 5, 6},
 			expectedInstructions: []code.Instructions{
 				code.Make(code.OpConstant, 0),
